c252173db02b3067cab5a2921c1feae9d924b2a9
UEM CSE (AI & ML) IEDC <iedc.cseaiml@uem.edu.in>
2025-10-19 11:23:46 +0000

    fix cors

--- image_gen_project/image_gen_project/settings.py
+++ image_gen_project/image_gen_project/settings.py
@@ -14,7 +14,7 @@ SECRET_KEY = 'django-insecure-1em)1o+u$#hdhuqf--rg4pg7ds+g!qjr8228&fy(b=&n_k!1g_
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -28,10 +28,13 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'image_gen_app.apps.ImageGenAppConfig'
+    'image_gen_app.apps.ImageGenAppConfig',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
+    "corsheaders.middleware.CorsMiddleware",
+    "django.middleware.common.CommonMiddleware",
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -40,6 +43,8 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
+CORS_ALLOW_ALL_ORIGINS = True
 
 ROOT_URLCONF = 'image_gen_project.urls'
 
